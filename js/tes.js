8414845e8f56316cd3a05f019220195bc5893ee6
poloprib-cloud <poloprib@gmail.com>
2026-02-17 03:17:22 +0700

    wkwk

---
+++ js/tes.js
@@ -0,0 +1,7 @@
+/**
+ * Minified by jsDelivr using UglifyJS v3.4.0.
+ * Original file: /gh/naptha/tesseract.js@1.0.10/dist/tesseract.js
+ * 
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{("undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:this).Tesseract=e()}}(function(){return function s(i,a,c){function u(r,e){if(!a[r]){if(!i[r]){var t="function"==typeof require&&require;if(!e&&t)return t(r,!0);if(l)return l(r,!0);var n=new Error("Cannot find module '"+r+"'");throw n.code="MODULE_NOT_FOUND",n}var o=a[r]={exports:{}};i[r][0].call(o.exports,function(e){var t=i[r][1][e];return u(t||e)},o,o.exports,s,i,a,c)}return a[r].exports}for(var l="function"==typeof require&&require,e=0;e
